8e58f8fe7b4072104481e939c59eeb48a8190138
hannah-developer <hannahchoi.developer@gmail.com>
2021-04-03 12:29:31 +0100

    refactoring

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ const recipeCol = document.querySelector(".recipeCol");
 const recipeUl = document.getElementById("recipeUl");
 const randomDiv = document.querySelector(".random");
 
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    getData(e.target.search.value);
-    e.target.search.value = "";
-});
-
+//get a list of meals matching with search input
 const getData = input => {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
         .then(res => res.json())
         .then(result => {
+            const searchResult = `<p>Search results for "${input}"</p>`;
+            if (result.meals === null) {
+                clearDom([randomDiv, recipeUl]);
+                searchResult + `<p>No result, please try again</p>`;
+            }
             const listItem = result.meals
                 .map(
                     item =>
@@ -26,50 +26,67 @@ const getData = input => {
                         `
                 )
                 .join("");
-            randomDiv.innerHTML = "";
-            recipeUl.innerHTML = listItem;
+            clearDom([randomDiv]);
+            recipeUl.innerHTML = searchResult + listItem;
         });
 };
 
+//when click an item in the list, fetch the exact meal
 const getDataById = id => {
-    fetch(
-        `https://www.themealdb.com/api/json/v1/1/${
-            id ? "lookup.php?i=" + id : "random.php"
-        }`
-    )
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${+id}`)
+        .then(res => res.json())
+        .then(result => {
+            if (result.meals) {
+                const data = result.meals[0];
+                addMealToDOM(data, "id");
+            }
+        });
+};
+
+//get a random meal
+const getRandomMeal = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
         .then(res => res.json())
         .then(result => {
             const data = result.meals[0];
-            let ingredientsArr = [];
-            for (let i = 0; i <= 20; i++) {
-                if (data["strIngredient" + i]) {
-                    ingredientsArr.push({
-                        name: data["strIngredient" + i],
-                        amount: data["strMeasure" + i],
-                    });
-                }
+            addMealToDOM(data, "random");
+        });
+};
+
+//render recipe to the DOM
+const addMealToDOM = (data, param) => {
+    let ingredientsArr = [];
+    for (let i = 1; i <= 20; i++) {
+        if (data[`strIngredient${i}`]) {
+            ingredientsArr.push({
+                name: `${data[`strIngredient${i}`]}`,
+                amount: `${data[`strMeasure${i}`]}`,
+            });
+        } else {
+            break;
+        }
+    }
+
+    const getIngredients = ingredientsArr
+        .map(ing =>
+            ing.amount !== null
+                ? isNaN(ing.amount)
+                    ? `<span>${ing.name} ${ing.amount}</span>`
+                    : `<span>${ing.amount} ${ing.name}</span>`
+                : `<span>${ing.name}</span>`
+        )
+        .join("");
+
+    const instructions = data.strInstructions
+        .split("\r\n")
+        .map((item, i) => {
+            if (item.length > 4) {
+                return `<p>${item}</p>`;
             }
+        })
+        .join("");
 
-            const getIngredients = ingredientsArr
-                .map(ing =>
-                    ing.amount !== null
-                        ? isNaN(ing.amount)
-                            ? `<span>${ing.name} ${ing.amount}</span>`
-                            : `<span>${ing.amount} ${ing.name}</span>`
-                        : `<span>${ing.name}</span>`
-                )
-                .join("");
-
-            const instructions = data.strInstructions
-                .split("\r\n")
-                .map((item, i) => {
-                    if (item.length > 4) {
-                        return `<p>${item}</p>`;
-                    }
-                })
-                .join("");
-
-            const recipe = `
+    const recipe = `
                 <h2>${data.strMeal}</h2>
                 <h4>${data.strArea}</h4>
                 <img src=${data.strMealThumb}>
@@ -77,19 +94,36 @@ const getDataById = id => {
                 <div class="instructions">${instructions}</div>
                 `;
 
-            if (id) {
-                randomDiv.innerHTML = "";
-                recipeCol.innerHTML = recipe;
-            } else {
-                recipeCol.innerHTML = "";
-                recipeUl.innerHTML = "";
-                randomDiv.innerHTML = recipe;
-            }
-        });
+    //When it's from search result
+    if (param === "id") {
+        clearDom([randomDiv]);
+        recipeCol.innerHTML = recipe;
+    } else {
+        //when it's from shuffle
+        clearDom([recipeCol, recipeUl]);
+        randomDiv.innerHTML = recipe;
+    }
 };
 
+const clearDom = arr => {
+    arr.forEach(item => (item.innerHTML = ""));
+};
+
+//events
+
 shuffleBtn.addEventListener("click", e => {
-    getDataById();
+    getRandomMeal();
+});
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    if (!e.target.search.value) {
+        alert("Type something");
+        return;
+    } else {
+        getData(e.target.search.value);
+        e.target.search.value = "";
+    }
 });
 
 recipeUl.addEventListener("click", e => {
